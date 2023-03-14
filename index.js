1da25b802711d0b6c88120af47793d53fc47fd54
Eric Krukowski <erickruk@amazon.com>
2023-03-13 23:49:31 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(dist){
+    return Math.abs(dist-42);
+}
+function distanceFromHqInFeet(dist){
+    return distanceFromHqInBlocks(dist)*264;
+}
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(start-destination)*264;
+}
+function calculatesFarePrice(start, destination){
+    const dist = distanceTravelledInFeet(start, destination);
+    if(dist<400) return 0;
+    else if (dist<2000) return .02*(dist-400);
+    else if (dist<2500) return 25;
+    else return 'cannot travel that far';
+}
